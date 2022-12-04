5960961921cff12e441d04ed885d4d7f45e25556
=dhonatagithup <=dhonata.borges2017@gmail.com>
2022-12-03 22:14:39 -0300

    Desafio 7.12

---
+++ src/test/java/com/borges/algafood/AlgafoodApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.borges.algafood;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgafoodApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
